477cb43586753bcfd6dd567b625c86d1af841c54
OwenColburn <136394955+OwenColburn@users.noreply.github.com>
2023-06-14 12:09:47 -0400

    Update Problem18.py
    Problem now solved

--- Problem18.py
+++ Problem18.py
@@ -34,34 +34,62 @@ NOTE: As there are only 16384 routes, it is possible to solve this problem by tr
 5. Do this for every row until you've done the top row.
 6. Return the value of the top row.
 """
+
+# Should take adjacent items in a list and compare them, returning the larger value
+def compare(list):
+   compare_one = list[0]
+   compare_two = list[1]
+
+   if compare_one >= compare_two:
+      return compare_one
+   else:
+      return compare_two
+   
+def check_stop(list, row):
+   if len(list) == row*2:
+      return True
+   return False
+
+
 test_array = [[3],[7,4],[2,4,6],[8,5,9,3]]
+real_array = [[75],
+              [95,64],
+              [17, 47, 82],
+              [18, 35, 87, 10],
+              [20, 4, 82, 47, 65],
+              [19, 1, 23, 75, 3, 34],
+              [88, 2, 77, 73, 7, 63, 67],
+              [99, 65, 4, 28, 6, 16, 70, 92],
+              [41, 41, 26, 56, 83, 40, 80, 70, 33],
+              [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+              [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+              [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+              [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+              [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+              [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+sum_list = []
 
-print(test_array[3][3])
+for i in reversed(range(len(real_array))):
+   for j in range(len(real_array[i])):
+      # Handle the last item in the row
+      if j == len(real_array[i]) -1 and i != 0:
+          sum_list.append(real_array[i][j] + real_array[i-1][j-1])
+      # Handle the first item in the row
+      elif j == 0 and i != 0:
+         sum_list.append(real_array[i][j] + real_array[i-1][j])
+      # Handle all other "in between" items in the row
+      elif j > 0 and j < len(real_array[i]) - 1 and i-j != 0:
+         sum_list.append(real_array[i][j] + real_array[i-1][j-1])
+         sum_list.append(real_array[i][j] + real_array[i-1][j])
 
-def main():
-    for i in reversed(range(len(test_array))):
-        sum_list = []
-        for j in range(0,len(test_array[i])-1):
-            # if array[i][j] is the first item in the array (j=0), compare it to i-1,j
-            # if array[i][j] is the last item in the array (j=len(array[i])-1), compare to i-1, j-1
-            # if array[i][j] is in the middle (0 < j < len(array[i])-1), compare to i-1, j and i-1, j-1;
-            print(f"i={i}, j={j}")
-            print(f"array[i][j] = {test_array[i][j]}")
-            try:
-                print(f"array[i-1][j] = {test_array[i-1][j]}")
-                print(f"sum = {test_array[i][j]+test_array[i-1][j]} \n")
-            except ValueError:
-                print(f"array[i-1][j-1] = {test_array[i-1][j-1]}")
-            if j == 0:
-                sum_list.append(test_array[i][j] + test_array[i-1][j])
+   if check_stop(sum_list, i):
+      index = 0
+      row = i
 
-            elif j == len(test_array[i])-1:
-                sum_list.append(test_array[i][j] + test_array[i-1][j-1])
+      while index <= row-1:
+         real_array[row-1][index] = compare(sum_list[0:2])
+         sum_list.pop(0)
+         sum_list.pop(0)
+         index+=1
 
-            elif 0 < j and j < len(test_array[i]):
-                sum_list.append(test_array[i][j] + test_array[i-1][j]) 
-                sum_list.append(test_array[i][j] + test_array[i-1][j-1])
-
-        print(f"sum_list: {sum_list} \n")
-
-#main()
+print("The maximum sum from top to bottom is:", real_array[0][0])
